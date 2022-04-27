edef759bc53ff4aafb1a85fd8d339062f3ec62fe
Fabian Lien <fabian.lien@gmail.com>
2022-04-27 08:33:56 +0000

    initial commit

---
+++ testing/test_run.py
@@ -0,0 +1,12 @@
+import unittest
+from run import even_number_of_evens
+
+
+class TestEvens(unittest.TestCase):
+
+    def test_function_returns_True(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
